d02c5b58ff3774be6f756e4b6ee17b775df96254
LaJeNa <79808377+LaJeNa@users.noreply.github.com>
2022-06-24 20:51:31 -0400

    added more API function

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
 
     let forecastHTML = `<div class="row">`;
@@ -41,7 +42,12 @@ function displayForecast() {
 forecastElement.innerHTML = forecastHTML;
 }
 
-
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(displayForecast);
+}
 
 
 function displayTemperature(response) {
@@ -64,7 +70,9 @@ function displayTemperature(response) {
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
 
+    getForecast(response.data.coord);
 }
+
 
 function search(city) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
@@ -112,4 +120,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
 
-displayForecast();
+
